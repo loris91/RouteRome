b786d747172caba191d5213516ac9f9a78e3cba4
FrappoMacBookAir <FrappoMacBookAir@air-di-stefano.homenet.telecomitalia.it>
2015-08-20 17:33:21 +0200

    Rimozione Luoghi Sgraditi

--- RoutesRome/src/persistence/LuogoDAO.java
+++ RoutesRome/src/persistence/LuogoDAO.java
@@ -8,8 +8,9 @@ public interface LuogoDAO {
 	
 	boolean insert(Luogo luogo);
 	
-	List<Luogo> findByCategoria(String tag);	
+	List<Luogo> findByCategoria(String tag, Integer rate);
 	
 	List<Luogo> findAll();
+
 
 }
